35039e6d5dafdde3ee6f54be87d683e7fe52ab13
Maksfin <dyrnoj@bk.ru>
2015-12-24 00:23:31 +0300

    typeset home page

---
+++ app/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+	if($('.popup').length) {
+		Popup.init();
+	}
+
+	$('.form').on('submit', function(e) {
+		e.preventDefault();
+	});
+
+	$('.slider').bxSlider({
+	 	auto: true,
+	 	autoHover: true,
+	 	mode: 'fade',
+	 	speed: 800,
+	});	
+}); // --> end
+
+
+// Модуль 
+var Popup = (function() {
+	var
+		_popups = $('.popup');
+
+	var _close = function() {
+		_popups.hide();
+	};
+
+	return {
+		init: function() {
+			$('.').on('click', function(e) {
+				e.preventDefault();
+			});
+		}
+	};
+})();
+
